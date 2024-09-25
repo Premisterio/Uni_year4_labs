2b9037b80d44d95a7ff0d5cab82118860ad62a0b
Volodymyr <premisterio007@gmail.com>
2024-09-25 19:32:14 +0300

    Deleted unnecessary code

--- lab3/gtrans.py
+++ lab3/gtrans.py
@@ -1,12 +1,4 @@
-import os
-from google.cloud import translate_v2 as translate
 from translation_package.mod1 import TransLate, LangDetect, LanguageList
-
-# Вставте шлях до вашого API ключа
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/Users/premisterio/Documents/Файли/Інші файли/Google Translate API/empyrean-willow-435911-b7-388878703184.json"
-
-# Ініціалізація Google Translate клієнта
-translate_client = translate.Client()
 
 # Перевірка функцій
 print(TransLate("Hello, where are your from", "en", "uk"))
